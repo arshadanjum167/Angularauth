94742dd1b9dd7a130917816325b72f439806e5e6
arshad <arshadrockingstar@gmail.com>
2019-02-12 00:32:53 +0530

    upto t17

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -9,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   private _registerUrl='http://localhost:3000/api/register'
+  private _loginUrl='http://localhost:3000/api/login'
 
   constructor(private http: HttpClient) { }
 
@@ -16,4 +17,10 @@ export class AuthService {
   {
     return this.http.post<any>(this._registerUrl, user)
   }
+
+  loginUser(user)
+  {
+    return this.http.post<any>(this._loginUrl,user)
+  }
 }
+
--- ngApp/src/app/login/login.component.ts
+++ ngApp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+   
+  loginUserData = {}
+  constructor(private _auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  loginUser()
+  {
+     //console.log(this.loginUserData);
+     this._auth.loginUser(this.loginUserData)
+       .subscribe(
+         res1=>console.log(res1),
+         err1=>console.log(err1)
+       )
   }
 
 }
