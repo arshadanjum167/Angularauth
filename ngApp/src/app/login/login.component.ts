3049dcc0577e1de3611e5b8ab1409d43ae48604f
arshad <arshadrockingstar@gmail.com>
2019-02-20 01:09:13 +0530

    upto t23

--- ngApp/src/app/login/login.component.ts
+++ ngApp/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,8 @@ import {AuthService} from '../auth.service';
 export class LoginComponent implements OnInit {
    
   loginUserData = {}
-  constructor(private _auth: AuthService) { }
+  constructor(private _auth: AuthService,
+              private _router: Router) { }
 
   ngOnInit() {
   }
@@ -19,7 +21,11 @@ export class LoginComponent implements OnInit {
      //console.log(this.loginUserData);
      this._auth.loginUser(this.loginUserData)
        .subscribe(
-         res1=>console.log(res1),
+        res => {
+          //console.log(res)
+          localStorage.setItem('token',res.token);
+          this._router.navigate(['/special']);
+        },
          err1=>console.log(err1)
        )
   }
